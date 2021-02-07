5ed85cfc8b8f1492bcd3eb396fb7edd3a0b986c8
Epic91 <hernandeze791@gmail.com>
2021-02-07 10:05:38 -0600

    successful lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //Grab all the necessary DOM elements
+
+  const newTaskForm = document.getElementById("create-task-form");
+
+  //attaching event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+  const createNewTask = event => {
+    event.preventDefault(); 
+    //above code stops the submit 
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+    //console.log(newTask)
+
+    appendNewTask(newTask);
+    event.target.reset();
+  };
+
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+  };
